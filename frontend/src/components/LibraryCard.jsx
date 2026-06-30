506afde94bfadc17501e2fe6fd75109b7d3c9044
EddumuPushkar <eddumupushkar13@gmail.com>
2026-06-30 12:20:04 +0530

    designed dashboard

---
+++ frontend/src/components/LibraryCard.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+function LibraryCard({ library, fetchLibraries }) {
+	const handleDelete = async () => {
+		try{
+			const res = await api.delete(`/admin/library/delete/${library._id}`);
+			if(res.status === 200){
+				fetchLibraries();
+			}
+		}
+		catch(err){
+			console.log(err);
+		}
+	}
+  return (
+        <div className="flex items-center justify-between rounded-xl bg-white p-5 shadow">
+
+            <h2 className="text-lg font-semibold">
+                {library.libraryName}
+            </h2>
+
+            <button
+                onClick={handleDelete}
+                className="rounded-lg bg-red-600 px-4 py-2 text-white hover:bg-red-700"
+            >
+                Remove
+            </button>
+
+        </div>
+    );
+}
+
+export default LibraryCard
